6ab7974842a224977774c841c8c5979ce8363c48
eramoss <edulramos@outlook.com>
2022-10-13 18:30:20 -0300

    resolvendo bugs do storybook

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -18,7 +18,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/ignite-lab-design-system/'
+      config.base = '/Design-System/'
     }
 
     return config
--- storybook-static/assets/preview.9d65d405.js
+++
@@ -1,2 +0,0 @@
-import{t as o}from"./index.b091e738.js";import"./iframe.ec86e48c.js";import"./es.map.constructor.b0b0d66e.js";import"./es.number.to-fixed.3e98b561.js";import"./index.78e8d9c8.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
-//# sourceMappingURL=preview.9d65d405.js.map
